5ee88f0a61108b9130c9e1a5cc764979be706659
HELAL Michou <helal.michou.works@gmail.com>
2017-10-08 13:25:43 +0200

    reformat code

--- utils/params_validators.py
+++ utils/params_validators.py
@@ -2,7 +2,7 @@ import jsonschema
 from jsonschema import Draft4Validator
 import params
 from src import hazel
-from conf.apiVariables import MAP_PARAMS,KEY_VALID_PARAMS,KEY_ERROR_SCHEMA
+from conf.apiVariables import MAP_PARAMS, KEY_VALID_PARAMS, KEY_ERROR_SCHEMA
 from utils import schema_validators
 
 
@@ -11,22 +11,22 @@ def put_all_valid_params():
     parameters = hazel.client.get_map(MAP_PARAMS)
     valid_params_hazel = parameters.get(KEY_VALID_PARAMS).result() if parameters.contains_key(KEY_VALID_PARAMS) else {}
     error_params_hazel = parameters.get(KEY_ERROR_SCHEMA).result() if parameters.contains_key(KEY_ERROR_SCHEMA) else {}
-    valid_params_hazel = valid_params_hazel if valid_params_hazel != None else {}
-    error_params_hazel = error_params_hazel if error_params_hazel != None else {}
+    valid_params_hazel = valid_params_hazel if valid_params_hazel is not None else {}
+    error_params_hazel = error_params_hazel if error_params_hazel is not None else {}
 
-    _schemas_hazel,_error_schema_hazel = schema_validators.get_hazel_schema()
-    for key_params in  _get_json_prams:
+    _schemas_hazel, _error_schema_hazel = schema_validators.get_hazel_schema()
+    for key_params in _get_json_prams:
         if key_params in _schemas_hazel:
             try:
-                jsonschema.validate(_get_json_prams[key_params],_schemas_hazel[key_params])
+                jsonschema.validate(_get_json_prams[key_params], _schemas_hazel[key_params])
                 valid_params_hazel[key_params] = _get_json_prams[key_params]
                 if key_params in error_params_hazel:
                     del error_params_hazel[key_params]
 
-            except Exception,e:
+            except Exception, e:
                 error_params_hazel[key_params] = {
-                    "params" : _get_json_prams[key_params],
-                    "error"  : str(e)
+                    "params": _get_json_prams[key_params],
+                    "error": str(e)
 
                 }
         else:
@@ -36,16 +36,17 @@ def put_all_valid_params():
 
             }
 
-
     parameters.put(KEY_VALID_PARAMS, valid_params_hazel)
     parameters.put(KEY_ERROR_SCHEMA, error_params_hazel)
+
 
 def get_hazel_params():
     parameters = hazel.client.get_map(MAP_PARAMS)
     valid_params_hazel = parameters.get(KEY_VALID_PARAMS).result() if parameters.contains_key(KEY_VALID_PARAMS) else {}
     error_params_hazel = parameters.get(KEY_ERROR_SCHEMA).result() if parameters.contains_key(KEY_ERROR_SCHEMA) else {}
-    valid_params_hazel = valid_params_hazel if valid_params_hazel != None else {}
-    error_params_hazel = error_params_hazel if error_params_hazel != None else {}
-    return valid_params_hazel,error_params_hazel
+    valid_params_hazel = valid_params_hazel if valid_params_hazel is not None else {}
+    error_params_hazel = error_params_hazel if error_params_hazel is not None else {}
+    return valid_params_hazel, error_params_hazel
+
 
 put_all_valid_params()
